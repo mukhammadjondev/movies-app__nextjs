6f16d383b869ebc34b211b128c62cf97a19fed5d
mukhammadjondev <muhsdev@gmail.com>
2023-11-09 20:30:24 +0500

    server side rendering

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
 import Header from "@/components/header";
+import { API_REQUEST } from "./services/api.service";
 
-export default function Home() {
+export default async function Home() {
+  const trending = await fetch(API_REQUEST.trending).then(res => res.json())
+  console.log(trending)
+
   return <div className="relative h-[200vh]">
       <Header />
       <main></main>
