04cde3fe9310c3fb4e2b69b13c6e9bd421b70443
mukhammadjondev <muhsdev@gmail.com>
2023-11-10 18:30:39 +0500

    hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
-import Header from "@/components/header";
-import { API_REQUEST } from "./services/api.service";
+import { Header, Hero } from "@/components";
+import { IMovie } from "@/interfaces/app.interface";
+import { API_REQUEST } from "../services/api.service";
 
 export default async function Home() {
-  const trending = await fetch(API_REQUEST.trending).then(res => res.json())
-  console.log(trending)
+  const res = await fetch(API_REQUEST.trending).then(res => res.json())
+  const trending: IMovie[] = res.results
 
   return <div className="relative h-[200vh]">
       <Header />
-      <main></main>
+      <main className="relative pl-4 pb-24 lg:space-y-24 lg:pl-16">
+        <Hero trending={trending} />
+      </main>
     </div>
 }
